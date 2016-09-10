a2535bc86faae2a1cd45ca234123c9a8bab33701
Yigit Ozkavci <yigitozkavci8@gmail.com>
2016-09-10 10:05:00 +0300

    Create tests for Hurriyet module and Client

---
+++ spec/libs/hurriyet/client_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+module Hurriyet
+  describe Client do
+    let(:apikey) { 'some_api_key' }
+    let(:client) { Client.new apikey }
+    let(:services) do
+      %w(articles columns news_photo_galleries pages paths writers)
+    end
+
+    it 'returns services correctly' do
+      services.each do |r|
+        expect(client).to respond_to(r)
+        service = client.send(r)
+        expect(service.class.name.split('::')[1]).to eq('Service')
+      end
+    end
+  end
+end
---
+++ spec/libs/hurriyet_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require 'hurriyet/version'
+
+describe Hurriyet::VERSION do
+  it { should match(/\d+\.\d+\.\d+/) }
+end
